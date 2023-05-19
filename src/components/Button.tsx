f8fd640e5b1acca5e7c0dff385d27511c7c0b8b6
Randall3475 <132885679+Randall3475@users.noreply.github.com>
2023-05-19 17:30:08 +0200

    Update Button.tsx

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -11,13 +11,13 @@ export default function Button({ type = '', text }: ButtonProps) {
       case 'style-3':
         return 'bg-gradient-to-b from-sr-gradient-first to-sr-gradient-second py-3.5 after:absolute after:top-0 after:left-0 after:w-full after:h-full after:bg-white after:opacity-0 hover:after:opacity-50';
       default:
-        return 'bg-pr-dark-purple desktop:py-3.5 hover:bg-pr-grayish-blue py-[11px] px-[24px]';
+        return 'bg-pr-dark-purple desktop:py-3.5 desktop:px-8 hover:bg-pr-grayish-blue py-[11px] px-[24px]';
     }
   }
   return (
     <>
       <button
-        className={`capitalize relative overflow-hidden transition-all font-bold px-8 text-[18px] leading-[28px] text-white rounded-full ${renderStyle(
+        className={`capitalize relative overflow-hidden transition-all px-8 font-bold text-[18px] leading-[28px] text-white rounded-full ${renderStyle(
           type
         )}`}
       >
